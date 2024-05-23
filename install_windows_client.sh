624d90f1324d529bda6d0ff86cdb4115ebc785a4
Chris Miller <git@chrismiller.xyz>
2024-05-22 18:31:47 -0700

    Update install_windows_client.sh

--- install_windows_client.sh
+++ install_windows_client.sh
@@ -8,6 +8,3 @@ cargo install --locked --root install --path . --target x86_64-pc-windows-gnu
 mkdir -p windows
 cp install/bin/quatrix.exe windows/quatrix.exe
 cp -r assets windows
-cp /usr/lib/gcc/x86_64-w64-mingw32/9.3-posix/libstdc++-6.dll windows
-cp /usr/lib/gcc/x86_64-w64-mingw32/9.3-posix/libgcc_s_seh-1.dll windows
-cp /usr/x86_64-w64-mingw32/lib/libwinpthread-1.dll windows
